4dda64f619dacb894a1831b3f63a12355d7a4b9d
Alexander Pazdnikov <apazdnikov@ptsecurity.com>
2020-12-18 09:35:49 +0300

    add stream writeand read desp_lock for unittests with stringio

--- crosspm/output_formatters/deps_txt_lock_list_formatter.py
+++ crosspm/output_formatters/deps_txt_lock_list_formatter.py
@@ -1,32 +1,33 @@
 from tabulate import tabulate
 
+
 class DepsTxtLockListFormatter:
     def __init__(self):
         pass
 
     @staticmethod
-    def write_packages_to_lock_file(deps_lock_file_path, packages):
+    def write_packages_to_lock_file(deps_lock_stream, packages):
 
-        packages_for_table = [[p.art_package.name, p.art_package.version, p.art_package.art_path.path_in_repo] for p in packages.values()]
+        packages_for_table = [[p.art_package.name, p.art_package.version, p.art_package.art_path.path_in_repo] for p in
+                              packages.values()]
         table = tabulate(packages_for_table, headers=[], tablefmt='plain')
 
-        with open(deps_lock_file_path, 'w+') as f:
-           f.write(table)
-
-        #
-        # with open(deps_lock_file_path, 'w+') as f:
-        #     for p in packages.values():
-        #         print(f"{p.art_package.name}    {p.art_package.}p.art_package.art_path.path_in_repo, file=f)
+        deps_lock_stream.write(table)
 
     @staticmethod
-    def read_packages_from_lock_file(deps_lock_file_path):
+    def read_packages_from_lock_file(deps_lock_stream):
 
         paths = []
 
-        with open(deps_lock_file_path) as f:
-            for l in f.read.splitlines():
-                p = l.split()
-                if p.len != 2:
-                    raise Exception(f"Line invalid:\n{l}\nExpect format of line as:\n<name> <version> <path>")
-                paths.append(l.split()[2])
+        for i, line in enumerate(deps_lock_stream.read().splitlines(), 1):
+            sline = line.strip()
+            if not sline or sline.startswith('#'):
+                continue
 
+            p = sline.split()
+            if len(p) != 3:
+                raise Exception(
+                    f"Line {i} invalid, expect 3 columns, but has {len(p)}:\n<<<{line}>>>\nsplitted into: {p}\nExpect format of line as:\n<name> <version> <path>")
+            paths.append(p[2])
+
+        return paths
---
+++ crosspm/output_formatters/test/test_deps_txt_lock_list_formatter.py
@@ -0,0 +1,6 @@
+from crosspm.output_formatters.deps_txt_lock_list_formatter import DepsTxtLockListFormatter
+
+
+def test_write_packages_to_lock_file():
+    DepsTxtLockListFormatter
+    assert False
