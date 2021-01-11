0ce70f13681743c757b064d13a53340496bf6784
Alexander Pazdnikov <apazdnikov@ptsecurity.com>
2021-01-11 16:40:36 +0300

    moved tests to best practice root test dir for pypi-package

--- tests/adapters/test_artifactoryaql2.py
+++ tests/adapters/test_artifactoryaql2.py
@@ -1,16 +1,7 @@
 import pytest
-from addict import Dict
 
-from contracts.contract import Contract
 from contracts.package import Package
-from crosspm.adapters.artifactoryaql2 import ArtifactoryAql2, remove_package_versions_with_missing_contracts
-
-
-def test_parse_contracts_from_items_find_results():
-    assert ArtifactoryAql2.parse_contracts_from_items_find_results([Dict({'key': 'contracts.db', 'value': '12'})]) \
-        == {'contracts.db': '12'}
-    assert ArtifactoryAql2.parse_contracts_from_items_find_results([Dict({'key': 'contracts.db', 'value': '2.5.230'})]) \
-        == {'contracts.db': '2.5.230'}
+from crosspm.adapters.artifactoryaql2 import remove_package_versions_with_missing_contracts
 
 @pytest.mark.parametrize(
     "test_case",
