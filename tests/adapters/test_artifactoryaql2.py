0357cd93d9efa39b109c0fc82bda7010ba87efd4
Alexander Pazdnikov <apazdnikov@ptsecurity.com>
2021-01-18 14:25:55 +0300

    fix contracts tests

--- tests/adapters/test_artifactoryaql2.py
+++ tests/adapters/test_artifactoryaql2.py
@@ -8,19 +8,19 @@ from crosspm.adapters.artifactoryaql2 import remove_package_versions_with_missin
     [
         {
             'package_versions': [('db', 4, [('c.db', 1)])],
-            'package_contracts': 'c.db',
+            'package_contracts': ['c.db'],
             'packages_with_all_contracts': [('db', 4, [('c.db', 1)])],
             'packages_with_missing_contracts': {}
         },
         {
             'package_versions': [('db', 4, [('c.db', 1)])],
-            'package_contracts': 'c.db;c.rest',
+            'package_contracts': ['c.db', 'c.rest'],
             'packages_with_all_contracts': [],
             'packages_with_missing_contracts': {('db', 4): ['c.rest']}
         },
         {
             'package_versions': [('db', 4, [('c.db', 1), ('c.rest', 1)])],
-            'package_contracts': 'c.db',
+            'package_contracts': ['c.db'],
             'packages_with_all_contracts': [('db', 4, [('c.db', 1)])],
             'packages_with_missing_contracts': {}
         },
@@ -31,7 +31,7 @@ from crosspm.adapters.artifactoryaql2 import remove_package_versions_with_missin
                  ('db', 6, []),
                  ('db', 7, [('c.rest', 1)])
             ],
-            'package_contracts': 'c.db',
+            'package_contracts': ['c.db'],
             'packages_with_all_contracts': [('db', 4, [('c.db', 1)]), ('db', 5, [('c.db', 1)])],
             'packages_with_missing_contracts': {('db', 6) : ['c.db'], ('db', 7) : ['c.db']}
         },
@@ -39,7 +39,7 @@ from crosspm.adapters.artifactoryaql2 import remove_package_versions_with_missin
 )
 def test_remove_package_versions_with_missing_contracts(test_case):
     package_versions = [Package.create_package(x) for x in test_case['package_versions']]
-    package_contracts = test_case['package_contracts'].strip().split(';')
+    package_contracts = test_case['package_contracts']
     expected_packages_with_all_contracts = [Package.create_package(x) for x in test_case['packages_with_all_contracts']]
     expected_packages_with_missing_contracts = {Package.create_package(p): c for p, c in test_case['packages_with_missing_contracts'].items()}
 
